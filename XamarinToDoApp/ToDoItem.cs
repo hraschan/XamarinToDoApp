7fea25703033f0b7f0595981e175e36312879f7e
hraschan <office@mhraschan.com>
2022-01-15 18:59:40 +0100

    Projektdateien hinzufügen.

---
+++ XamarinToDoApp/ToDoItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace XamarinToDoApp
+{
+    internal class ToDoItem
+    {
+        public string todoText { get; set; }
+        public bool completed { get; set; }
+
+        public ToDoItem(string todoText, bool completed)
+        {
+            this.todoText = todoText;
+            this.completed = completed;
+        }
+    }
+}
---
+++ XamarinToDoApp/TodoListViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace XamarinToDoApp
+{
+    internal class TodoListViewModel
+    {
+        public ObservableCollection<ToDoItem> todoItems { get; set; }
+
+        public ICommand AddTodoCommand => new Command(AddTodoItem);
+        public ICommand DeleteTodoCommand => new Command(DeleteTodoItem);
+
+        public string NewTodoInputValue { get; set; }
+
+        public TodoListViewModel()
+        {
+           todoItems = new ObservableCollection<ToDoItem>();
+
+           todoItems.Add(new ToDoItem("todo 1", false));
+           todoItems.Add(new ToDoItem("todo 2", true));
+
+        }
+
+        void AddTodoItem()
+        {
+            todoItems.Add(new ToDoItem(NewTodoInputValue, false));
+
+
+        }
+
+        void DeleteTodoItem(object item)
+        {
+            ToDoItem todoItemBeingRemoved = item as ToDoItem;
+            todoItems.Remove(todoItemBeingRemoved);
+        }
+
+    }
+}
